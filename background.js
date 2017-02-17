18d4d7dc99e89debf1b9d5de19b1c76d63a4dfe3
Michiel Verkoijen <michielverkoijen@voormedia.com>
2017-02-17 16:18:08 +0100

    Add checks if we have a useful URL to analyze.

--- background.js
+++ background.js
@@ -1,7 +1,21 @@
 function openAnalyzerPage(url) {
-  chrome.tabs.create({
-    url: 'https://tinypng.com/analyzer#' + url
-  });
+  if (validSiteURL(url)) {
+    chrome.tabs.create({
+      url: 'https://tinypng.com/analyzer#' + url
+    });
+  }
+}
+
+function validSiteURL(url) {
+  if (url.substring(0, 28) == 'https://tinypng.com/analyzer' ) {
+    return false
+  } else if (url.substring(0, 8) == 'https://' ) {
+    return true
+  } else if (url.substring(0, 7) == 'http://' ) {
+    return true
+  } else {
+    return false
+  }
 }
 
 chrome.browserAction.onClicked.addListener((tab) => {
